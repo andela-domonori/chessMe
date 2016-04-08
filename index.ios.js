5030dcf908435a0d993217de43e83b33d15a9d4c
Omonori Damilola Jesmine <damilola.omonori@andela.co>
2016-04-08 05:54:15 +0000

    board arrangment

--- index.ios.js
+++ index.ios.js
@@ -11,20 +11,68 @@ import React, {
   View
 } from 'react-native';
 
+import {List, Range} from 'immutable'
+
+// import Board from './src/components/Board'
+
+
+class Board extends Component {
+  constructor(props) {
+    super(props)
+    this.piecesToRow = this.piecesToRow.bind(this)
+
+    //Board creation
+    const pieces = List(['rook','knight','bishop','king','queen','bishop','knight','rook'])
+    const whitePieces = pieces
+    const blackPieces = pieces
+    const emptyRow = Range(0,8).map(() => null)
+    const pawnRow = emptyRow.map(() => 'pawn')
+    const whitePiecesRow = this.piecesToRow(whitePieces, true)
+    const blackPiecesRow = this.piecesToRow(blackPieces, false)
+    const whitePawnRow = this.piecesToRow(pawnRow, true)
+    const blackPawnRow = this.piecesToRow(pawnRow, false)
+    const board = List([
+      blackPiecesRow,
+      blackPawnRow,
+      emptyRow,
+      emptyRow,
+      emptyRow,
+      emptyRow,
+      whitePawnRow,
+      whitePiecesRow
+    ])
+
+    this.state = {
+      board
+    }
+  }
+
+  piecesToRow(pieces, white) {
+    return pieces.map(x => {
+      return {
+       white,
+       type: x
+      }
+    })
+  }
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.welcome}>
+          Board View
+        </Text>
+      </View>
+    )
+  }
+}
+
+
 class chessMe extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Board />
       </View>
     );
   }
---
+++ src/components/Board.jsx
@@ -0,0 +1,21 @@
+import React from 'react-native'
+
+import {
+  Component,
+  View,
+  Text
+} = React
+
+class Board extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Board View
+        </Text>
+      </Viw>
+    )
+  }
+}
+
+export default Board
